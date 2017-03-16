f33577fe3a0c21c7ba01483234a44daa309a53e2
Jem Davis <jemd321+programming@gmail.com>
2017-03-16 11:42:58 +0000

    Added protein molecular weight calculation method to the MW calculator.

--- BioinformaticsSuite.Module/Models/MolecularWeightCalculator.cs
+++ BioinformaticsSuite.Module/Models/MolecularWeightCalculator.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using BioinformaticsSuite.Module.Enums;
 using BioinformaticsSuite.Module.Utility;
+using Microsoft.Practices.ObjectBuilder2;
 
 namespace BioinformaticsSuite.Module.Models
 {
@@ -27,18 +28,43 @@ namespace BioinformaticsSuite.Module.Models
         private const decimal RnaUWeight = 306.17m;
         private const decimal RnaWeightCorrection = 159;
 
+        private const decimal ProteinWaterWeight = 18.02m;
+        private static readonly Dictionary<char, decimal> AminoAcidWeights = new Dictionary<char, decimal>()
+        {
+            {'A', 71.08m},
+            {'C', 103.14m},
+            {'D', 115.09m},
+            {'E', 129.12m},
+            {'F', 147.18m},
+            {'G', 57.06m},
+            {'H', 137.15m},
+            {'I', 113.17m},
+            {'K', 128.18m},
+            {'L', 113.17m},
+            {'M', 131.21m},
+            {'N', 114.11m},
+            {'P', 97.12m},
+            {'Q', 128.41m},
+            {'R', 156.20m},
+            {'S', 87.08m},
+            {'T', 101.11m},
+            {'V', 99.14m},
+            {'W', 186.21m},
+            {'Y', 163.18m}
+        };
+
         public void CalculateMolecularWeight(LabelledSequence labelledSequence)
         {
             switch (labelledSequence.SequenceType)
             {
                  case SequenceType.Dna:
-                    labelledSequence.MolecularWeight = CalculateDnaMolecularWeight(labelledSequence as Dna);
+                    labelledSequence.MolecularWeight = CalculateDnaMolecularWeight((Dna)labelledSequence);
                     break;
                  case SequenceType.Rna:
-                    labelledSequence.MolecularWeight = CalculateRnaMolecularWeight(labelledSequence as Rna);
+                    labelledSequence.MolecularWeight = CalculateRnaMolecularWeight((Rna)labelledSequence);
                     break;
                 case SequenceType.Protein:
-                    labelledSequence.MolecularWeight = CalculateProteinMolecularWeight(labelledSequence as Protein);
+                    labelledSequence.MolecularWeight = CalculateProteinMolecularWeight((Protein)labelledSequence);
                     break;
                 default:
                     throw new Exception("labelled sequence type not correctly labelled");
@@ -47,11 +73,12 @@ namespace BioinformaticsSuite.Module.Models
 
         private static decimal CalculateDnaMolecularWeight(Dna dna)
         {
+            // Add up the total number of each base in the sequence, then multiply by base weight, then apply weight correction
             int[] baseCount = dna.Sequence.CountDnaBases();
-            int aCount = baseCount[0];
-            int cCount = baseCount[1];
-            int gCount = baseCount[2];
-            int tCount = baseCount[3];
+            var aCount = baseCount[0];
+            var cCount = baseCount[1];
+            var gCount = baseCount[2];
+            var tCount = baseCount[3];
             decimal molecularWeight = (aCount*DnaAWeight) + (cCount*DnaCWeight) + (gCount*DnaGWeight) +
                                       (tCount*DnaTWeight) - DnaWeightCorrection;
             return molecularWeight;
@@ -60,10 +87,10 @@ namespace BioinformaticsSuite.Module.Models
         private static decimal CalculateRnaMolecularWeight(Rna rna)
         {
             int[] baseCount = rna.Sequence.CountRnaBases();
-            int aCount = baseCount[0];
-            int cCount = baseCount[1];
-            int gCount = baseCount[2];
-            int uCount = baseCount[3];
+            var aCount = baseCount[0];
+            var cCount = baseCount[1];
+            var gCount = baseCount[2];
+            var uCount = baseCount[3];
             decimal molecularWeight = (aCount * RnaAWeight) + (cCount * RnaCWeight) + (gCount * RnaGWeight) +
                                       (uCount * RnaUWeight) + RnaWeightCorrection;
             return molecularWeight;
@@ -71,8 +98,22 @@ namespace BioinformaticsSuite.Module.Models
 
         private static decimal CalculateProteinMolecularWeight(Protein protein)
         {
-            // NOT IMPLEMENTED YET!
-            return 12;
+            string sequence = protein.Sequence;
+            decimal molecularWeight = 0;
+
+            var aminoAcidCount = sequence.CountAminoAcids();
+            foreach (var count in aminoAcidCount)
+            {
+                decimal weight;
+                if (!AminoAcidWeights.TryGetValue(count.Key, out weight))
+                {
+                    throw new Exception("Amino acid key not found, have invalid amino acids made it through UI input validation?");
+                }
+                molecularWeight += count.Value*weight;
+            }
+            // Correct for the weight of water
+            molecularWeight += ProteinWaterWeight;
+            return molecularWeight;
         }
     }
 }
